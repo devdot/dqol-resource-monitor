33af1acc396bd68393a84c80becf5cfde2b831a0
Thomas Kuschan <mail@thomaskuschan.de>
2025-05-11 22:38:25 +0200

    Housekeeping

--- commands/commands.lua
+++ commands/commands.lua
@@ -4,8 +4,6 @@ if _DEBUG == true then
     commands.add_command('dqol-resource-monitor-reset-sites', 'debug tool', require('reset-sites'))
 end
 
-commands.add_command('dqol-resource-monitor-remove-resource-labels-mod', 'Remove the labels from Resource Labels Updated', require('remove-resource-labels-mod'))
-
 if script.active_mods['pypetroleumhandling'] then
     commands.add_command('dqol-resource-monitor-py-rescan-bitumen-seeps',
     'Re-Scan all sites with bitumen seep from the Pyanodons mod', require('py-refresh-bitumen-seeps'))
--- commands/remove-resource-labels-mod.lua
+++
@@ -1,7 +0,0 @@
-return function()
-    if script.active_mods['resourceMarker'] then
-        game.print('please run command: resourcemarker delete')
-    else
-        game.print('Mod Resource Labels Updated is not installed, cannot remove their labels!')
-    end
-end
